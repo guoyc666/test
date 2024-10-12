9b465759ef3e1508c45351f4e0ef95e7f5a58b92
GuoYanchen <18435349839@163.com>
2024-10-12 14:29:58 +0800

    init

---
+++ src/main/java/Plant.java
@@ -0,0 +1,10 @@
+public class Plant {
+    private final String name;
+    private int num;
+
+    public Plant(String name, int num) {
+        this.name = name;
+        this.num = num;
+    }
+
+}
